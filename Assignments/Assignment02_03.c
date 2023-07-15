0253903e7ce1bb151347ae6c0e736ff150ecc0cf
prasadsarwar <prasadsarwar02@gmail.com>
2023-07-16 02:40:38 +0530

    accept directory name from user and print all file names from that directory

---
+++ Assignments/Assignment02_03.c
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// write a program to accept directory name from user and print all     //
+// file names of that directory                                         //
+//                                                                      //
+// Usage : ./exe file_name "string"                                     //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dp = NULL;
+    struct dirent *entry = NULL;
+
+	if(argc != 2)
+	{
+		printf("Insufficient number of arguments\n");
+		return -1;
+	}
+
+	dp = opendir(argv[1]);
+
+	if(dp == NULL)
+	{
+		printf("Unable to open directory\n");
+		return -1;
+	}
+
+	while((entry = readdir(dp)) != NULL)
+	{
+		printf("%s\n",entry->d_name);
+	}
+
+	closedir(dp);
+
+	return 0;
+}
