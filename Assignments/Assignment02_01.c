fde8b3853f7c6f1ab31abe7e36b0b17982fc82c2
prasadsarwar <prasadsarwar02@gmail.com>
2023-07-16 01:59:33 +0530

    accept file name from user and read whole file

---
+++ Assignments/Assignment02_01.c
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //  
+// write a program to accept file name from user and read whole file    //
+//                                                                      //                            
+// Usage : ./exe file_name                                              //                                        
+//                                                                      //  
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+    int iRet = 0;
+    char Buffer[1024];
+
+    if(argc != 2)
+    {
+        printf("Insufficiient number of bytes\n");
+        return -1;
+    }
+
+    fd = open(argv[1],O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return -1;
+    }
+
+    while((iRet = read(fd,Buffer,sizeof(Buffer))) != NULL)
+    {
+        write(1,Buffer,iRet);
+    }
+
+    printf("\n");
+    
+    close(fd);
+
+    return 0;
+}
