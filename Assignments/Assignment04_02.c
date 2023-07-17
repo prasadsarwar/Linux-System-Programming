e0a4e47834b987a27257bdbb3da16a570d3b0438
prasadsarwar <prasadsarwar02@gmail.com>
2023-07-18 01:37:31 +0530

    accept name of directory and create directory of that name

---
+++ Assignments/Assignment04_02.c
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// write a program which accept name of directory and create directory  //
+// of that name                                                         //
+//                                                                      //
+// Usage : ./exe < output.txt > input.txt                               //
+//                                                                      //
+// Input.txt : 10 11                                                    //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    struct stat sobj;
+    int iRet = 0;
+
+    if(argc != 2)
+    {
+        printf("Invalid number of arguments\n");
+        return -1;
+    }
+
+///////////////////////////////////////////////////
+//  optional
+    stat(argv[1],&sobj);
+
+    if(S_ISDIR(sobj.st_mode))
+    {
+        printf("Directory already exist\n");
+        return -1;
+    }
+///////////////////////////////////////////////////
+    
+    iRet = mkdir(argv[1],0777);
+
+    if(iRet == -1)
+    {
+        printf("Unable to create directory\n");
+        return -1;
+    }
+    else
+    {
+        printf("Directory created successfully\n");
+    }
+
+    return 0;
+}
